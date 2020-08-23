15bdad8f43d60fd80fbe7f5ec21f8d54c49a7ed0
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2020-08-23 13:19:57 +0900

    add date

--- make.py
+++ make.py
@@ -19,17 +19,17 @@ data = []
 
 for i in range(48):
     assert wr_data[i][0] == my_data[i][0]
-    data.append((wr_data[i][0], to_sec(wr_data[i][1]), to_sec(my_data[i][1])))
+    data.append((wr_data[i][0], to_sec(wr_data[i][1]), to_sec(my_data[i][1]), '' if len(my_data[i]) == 2 else my_data[i][2]))
 
 def to_str(sec):
     sec = int(sec * 1000)
     return '{}\'{:02d}"{:03d}'.format(sec // 1000 // 60, sec // 1000 % 60, sec % 1000)
 
 def make_markdown_table(data):
-    ret = '|コース名|自己記録|WRとの差|\n'
-    ret += '|--|--|--|\n'
+    ret = '|コース名|自己記録|WRとの差|日付\n'
+    ret += '|--|--|--|--|\n'
     for t in data:
-        ret += '|{}|{}|{:.3f}|\n'.format(t[0], to_str(t[2]), t[1] - t[2])
+        ret += '|{}|{}|+{:.3f}|{}|\n'.format(t[0], to_str(t[2]), t[2] - t[1], t[3])
     ret += '\n'
     ret += 'Last updated: {}\n'.format(datetime.date.today())
     return ret
